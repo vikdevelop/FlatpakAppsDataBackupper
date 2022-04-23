f4ef0f20dcb95b197c9ff610919f8afb047247ad
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-04-23 18:14:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ date = date.today()
 
 class Dialog_export(Gtk.Dialog):
     def __init__(self, parent):
-        super().__init__(title="Export data of Flatpak applications", transient_for=parent, flags=0)
+        super().__init__(title="Package list has been created!", transient_for=parent, flags=0)
         self.add_buttons(
             Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OK, Gtk.ResponseType.OK
         )
@@ -18,12 +18,12 @@ class Dialog_export(Gtk.Dialog):
         self.set_default_size(150, 100)
 
         label = Gtk.Label()
-        label.set_markup("<b>Done!</b> Data has been exported successfully. Is in the directory: <i>%s/Flatpak_Apps_Data/Flatpak_Apps_Data_%s.tar.gz</i>" % (HOME, date)
+        label.set_markup("<b>Done!</b> Data of Flatpak apps has been exported successfully! File is in: <i>%s/Flatpak_Apps_Data/Flatpak_Apps_Data_%s.tar.gz</i>" % (HOME, date))
 
         box = self.get_content_area()
         box.add(label)
         self.show_all()
-        
+
 class Dialog_import(Gtk.Dialog):
     def __init__(self, parent):
         super().__init__(title="Import data of Flatpak applications", transient_for=parent, flags=0)
@@ -36,7 +36,7 @@ class Dialog_import(Gtk.Dialog):
         label = Gtk.Label()
         label.set_markup("<b>Done!</b>Data has been imported on your OS successfully! Data has been imported successfully to: <i>%s/.var/app/</i>" % HOME)
 
-        box = self.get_content_area()
+        box2 = self.get_content_area()
         box.add(label)
         self.show_all()
 
@@ -47,18 +47,18 @@ class PKGBackerWindow(Gtk.Window):
 
         mainBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
         self.add(mainBox)
-        
+
         self.label = Gtk.Label()
         mainBox.pack_start(self.label, True, True, 0)
-        
+
         self.label_h1 = Gtk.Label()
         self.label_h1.set_markup("<b>Export Flatpak Apps Data</b>")
         mainBox.pack_start(self.label_h1, True, True, 0)
-        
+
         self.button_export = Gtk.Button(label="Export")
         self.button_export.connect("clicked", self.on_button_export)
         mainBox.pack_start(self.button_export, True, True, 0)
-        
+
         self.label_h2 = Gtk.Label()
         self.label_h2.set_markup("<b>Import Flatpak Apps Data</b>")
         mainBox.pack_start(self.label_h2, True, True, 0)
@@ -66,7 +66,7 @@ class PKGBackerWindow(Gtk.Window):
         self.entry = Gtk.Entry()
         self.entry.set_placeholder_text("Enter directory to archive with imported flatpak apps data (eg. /home/user/Flatpak)")
         mainBox.pack_start(self.entry, True, True, 0)
-        
+
         self.button_importb = Gtk.Button(label="Import")
         self.button_importb.connect("clicked", self.on_button_importb)
         mainBox.pack_start(self.button_importb, True, True, 0)
@@ -74,7 +74,7 @@ class PKGBackerWindow(Gtk.Window):
     def on_button_export(self, widget, *args):
         """ button "clicked" in event buttonStart. """
         self.export()
-        
+
     def on_button_importb(self, widget, *args):
         self.importb()
 
@@ -102,7 +102,7 @@ class PKGBackerWindow(Gtk.Window):
             print("The Cancel button was clicked")
 
         dialog_e.destroy()
-        
+
     def importb(self):
         entry = self.entry.get_text()
         os.chdir("%s" % entry)
@@ -120,7 +120,7 @@ class PKGBackerWindow(Gtk.Window):
             print("The Cancel button was clicked")
 
         dialog_m.destroy()
-        
+
 win = PKGBackerWindow()
 win.show_all()
 win.connect("delete-event", Gtk.main_quit)
